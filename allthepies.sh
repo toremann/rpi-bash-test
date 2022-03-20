82d4101e7f91d8d52d976b70087285c6f06bb070
toremann <50668920+toremann@users.noreply.github.com>
2022-03-20 18:13:16 +0100

    Update allthepies.sh
    added arp-scan for millheat

--- allthepies.sh
+++ allthepies.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 #arp scan localnet to find rpi mac's
+echo "Scanning for RPI's:"
 sudo arp-scan --localnet --interface=eth0 | grep b8:27:eb:
+
+#arp scan for millheat
+echo "Scanning for Millheat:"
+sudo arp-scan --localnet --interface=eth0 | grep f0:fe:6b:
